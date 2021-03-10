83583739e80b3bea93dc19944930b6d4b6cde591
Manan <mehta.manan@aol.com>
2021-03-11 01:45:22 +0530

    init

---
+++ Divisible_sum_pairs.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Brute force
+int divisibleSumPairs(int n, int k, vector<int> ar)
+{
+    int count = 0;
+
+    for (int i = 0; i < ar.size(); i++)
+    {
+        for (int j = i + 1; j < ar.size(); j++)
+        {
+            if ((ar[i] + ar[j]) % k == 0)
+                count++;
+        }
+    }
+    
+    return count;
+}
