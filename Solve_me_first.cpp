a41439b7c77c1a3a980989ffd0b94a3c8a0693a2
Manan <mehta.manan@aol.com>
2021-03-15 16:11:51 +0530

    init

---
+++ Solve_me_first.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int solveMeFirst(int a, int b)
+{
+    return a + b;
+}
---
+++ Subarray_division.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split(const string &);
+
+int birthday(vector<int> s, int d, int m)
+{
+    int count = 0;
+    for (auto it = s.begin(); it + m <= s.end(); it++)
+    {
+        if (std::accumulate(it, it + m, 0) == d)
+            count++;
+    }
+    return count;
+}
