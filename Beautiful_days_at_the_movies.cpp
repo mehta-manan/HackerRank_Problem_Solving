d5499925bfeb1bcc9ee94fe6c14695b1c27cb7e2
Manan <mehta.manan@aol.com>
2021-03-19 15:57:32 +0530

    init

---
+++ Beautiful_days_at_the_movies.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int beautifulDays(int i, int j, int k)
+{
+    int c = 0;
+    
+    for (; i <= j; i++)
+    {
+        string rev_str = to_string(i);
+        std::reverse(rev_str.begin(), rev_str.end());
+        int rev_i = stoi(rev_str);
+
+        if (abs(rev_i - i) % k == 0)
+            c++;
+    }
+
+    return c;
+}
